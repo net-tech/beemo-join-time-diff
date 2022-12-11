5e03516d2433c0bd8e552f529c93414c46acd394
net-tech <hello@nettech.link>
2022-12-11 17:57:25 +0100

    feat: add loading bar
    Signed-off-by: net-tech <hello@nettech.link>

--- src/services/time.ts
+++ src/services/time.ts
@@ -1,10 +1,31 @@
-import { Stopwatch } from "@sapphire/stopwatch"
-import TextService from "./text"
 import { log } from "./logger"
-import { timeDifferenceResult } from "../types/time"
-import { logDomains } from "../types/general"
+import { logDomains, timeDifferenceResult } from "../types/master"
+import cliProgress from "cli-progress"
+import axios from "axios"
 
 class TimeService {
+
+	/**
+	 * Gets text from a URL.
+	 * @param url The URL to get the text from.
+	 * @returns The text from the URL.
+	 */
+	public static getText(url: string) {
+		log.debug(`Getting text from ${url}.`)
+		return new Promise((resolve, reject) => {
+			axios
+				.get(url)
+				.then((response) => {
+					log.debug(`Got text from ${url}.`)
+					resolve(response.data)
+				})
+				.catch((error) => {
+					log.error(error, `Error getting text from ${url}.`)
+					reject(error)
+				})
+		})
+	}
+
 	public static async timeDifference(
 		url: string
 	): Promise<timeDifferenceResult> {
@@ -15,7 +36,7 @@ class TimeService {
 			process.exit(1)
 		}
 
-		const text = await TextService.getText(url)
+		const text = await this.getText(url)
 			.then((text) => text as string)
 			.catch((error) => {
 				log.fatal(error, "Error getting text from URL.")
@@ -23,8 +44,6 @@ class TimeService {
 			})
 
 		log.debug("Got text from URL.")
-
-		const time = new Stopwatch()
 
 		const times = text.match(/\d\d:\d\d:\d\d\.\d\d\d-\d\d\d\d/g)
 		const zeros: number[] = []
@@ -35,28 +54,29 @@ class TimeService {
 			process.exit(1)
 		}
 
-		const dates = times.map(
-			(time) =>
-				new Date(
-					Date.parse(
-						`${new Intl.DateTimeFormat("en-CA", {
-							timeZone: "America/Los_Angeles",
-						}).format(Date.now())}T${time.replace(/(\d\d)(\d\d)$/, "$1:$2")}`
-					)
+		const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+		bar1.start(times.length, 0)
+		
+		const dates: Date[] = []
+		for (let i = 0; i < times.length; i++) {
+			bar1.increment()
+			const date = new Date(
+				Date.parse(
+					`${new Intl.DateTimeFormat("en-CA", {
+						timeZone: "America/Los_Angeles",
+					}).format(Date.now())}T${times[i].replace(/(\d\d)(\d\d)$/, "$1:$2")}`
 				)
-		)
+			)
+			dates.push(date)
+		}
 
 		for (let i = 1; i < dates.length; i++) {
+			bar1.update(i + 1)
 			const diff = dates[i].getTime() - dates[i - 1].getTime()
 			diffs.push(diff)
 			if (diff === 0) {
 				zeros.push(i)
 			}
-			log.debug(
-				`Difference between join time of ID ${times[i]} and ID ${
-					times[i - 1]
-				} is ${diff}ms.`
-			)
 		}
 
 		const zerosList =
@@ -65,7 +85,8 @@ class TimeService {
 				: zeros.join(", ")
 		const chance = zeros.length / diffs.length
 		const average = diffs.reduce((a, b) => a + b) / diffs.length
-		time.stop()
+
+		bar1.stop()
 
 		return {
 			averageTime: {
@@ -79,7 +100,6 @@ class TimeService {
 				indexArray: zeros,
 			},
 			joinCount: diffs.length,
-			timeTaken: time.toString(),
 		}
 	}
 }
